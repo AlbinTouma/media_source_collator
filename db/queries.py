b9f9665e4d71765cc5b90e9605bf1ee92c515bec
Albin Touma <albin.touma@complyadvantage.com>
2024-08-13 15:47:59 +0300

    init

--- db/queries.py
+++ db/queries.py
@@ -1,25 +1,3 @@
-temp_table = f"""
-CREATE TEMPORARY TABLE country_review (
-    uuid TEXT,
-    source_name TEXT,
-    url TEXT,
-    host TEXT, 
-    country TEXT, 
-    article_created_datetime TEXT,
-    collection_datetime TEXT, 
-    publish_datetime TEXT
-    );
-
-INSERT INTO country_review
-SELECT uuid, source_name, url, host, country, article_created_datetime, collection_datetime, publish_datetime
-FROM articles_with_mentions
-WHERE country = %s;
-"""
-
-find_article = f"""
-SELECT * from incoming_articles where host like %s and title like %s;
-"""
-
 get_db_domains = f"""
-    SELECT * FROM website WHERE country = %s;
+    SELECT * FROM {secret_table} WHERE {secret_col} = %s;
 """
